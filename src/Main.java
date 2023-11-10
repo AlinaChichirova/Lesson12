3f8695751d0fb5004a83672461b75f693d28454b
Alina Chichirova <privet-alina2014@yandex.ru>
2023-11-10 18:04:26 +0300

    Lesson12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> arrayList=new ArrayList<>();
+        List<Integer> linkedList=new LinkedList<>();
+        TreeSet<Integer> treeSet=new TreeSet<>();
+        HashSet<Integer> hashSet=new HashSet<>();
+        System.out.println(add(linkedList));
+        System.out.println(add(arrayList));
+        System.out.println(add2(treeSet));
+        System.out.println(add2(hashSet));
+
     }
+    public static long add(List <Integer> list){
+        long time=System.currentTimeMillis();
+        for (int i = 0; i < 500000; i++) {
+            list.add(i);
+        }
+        time=System.currentTimeMillis()-time;
+        return time;
+    }
+    public static long add2(Set<Integer> set){
+        long time=System.currentTimeMillis();
+        for (int i = 0; i < 500000; i++) {
+            set.add(i);
+        }
+        time=System.currentTimeMillis()-time;
+        return time;
+    }
+
+
 }
